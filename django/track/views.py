891eee228b7e83a4a292a423c71dd8683444a7a4
Tommy Falgout <tommy@lastcoolnameleft.com>
2018-12-27 11:38:34 -0600

    fix maps

--- django/track/views.py
+++ django/track/views.py
@@ -15,14 +15,14 @@ from haversine import haversine
 def index(request):
     """ / path """
     toy_location_list = ToyLocation.objects.all()
-    #toy_location_json = serializers.serialize('json', toy_location_list)
+    toy_location_json = serializers.serialize('json', toy_location_list)
     map_data = {
         'width': '100%',
         'height': '800px',
         'focus_lat': 23,
         'focus_long': 10,
         'focus_zoom': 2,
-        'location_list': [], #toy_location_json,
+        'location_list': toy_location_json,
         'toy_location_id': 0,
     }
 
