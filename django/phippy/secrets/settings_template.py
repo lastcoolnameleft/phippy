cd55aca90a4fdd08d3417ff2e3a4b3531ff584d0
Tommy Falgout <tommy@lastcoolnameleft.com>
2018-12-27 23:59:19 -0600

    add settings template

---
+++ django/phippy/secrets/settings_template.py
@@ -0,0 +1,63 @@
+""" Variable config settings.  Not to be checked in """
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = ''
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = ''
+
+SOCIAL_AUTH_FACEBOOK_KEY = ''
+SOCIAL_AUTH_FACEBOOK_SECRET = ''
+
+FLICKR_API_KEY = ''
+FLICKR_API_SECRET = ''
+FLICKR_AUTH_FILE = '/code/duckiehunt/secrets/authfile'
+FLICKR_PHOTO_IS_PUBLIC = '0'
+
+UPLOAD_PATH = 'uploads/'
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = ''
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# Database
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'phippy',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
+    }
+}
+
+# https://github.com/python-social-auth/social-app-django/issues/132#issuecomment-375186943
+SOCIAL_AUTH_REDIRECT_IS_HTTPS = False
+
+LOGGING = {
+    'disable_existing_loggers': False,
+    'version': 1,
+    'handlers': {
+        'console': {
+            # logging handler that outputs log messages to terminal
+            'class': 'logging.StreamHandler',
+            'level': 'DEBUG', # message level to be written to console
+        },
+    },
+    'loggers': {
+        '': {
+            # this sets root level logger to log debug and higher level
+            # logs to console. All other loggers inherit settings from
+            # root level logger.
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False, # this tells logger to send logging message
+                                # to its parent (will send if set to True)
+        },
+        'django.db.backends': {
+            'level': 'DEBUG',
+            'handlers': ['console', ],
+        },
+    },
+}
